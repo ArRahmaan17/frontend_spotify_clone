2b3735d7e7aeaff1dbaa2736cfbbaa5a4708d7f6
RahmaanM17 <ardrah17@gmail.com>
2025-01-09 00:38:12 +0700

    feat: update ui and ux on navbar

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ChartBarIcon as ActiveCharBarIcon } from "@heroicons/react/24/solid";
+import { ChartBarIcon as ActiveCharBarIcon, ArrowLeftIcon } from "@heroicons/react/24/solid";
 import {
   ChartBarIcon,
   PlusIcon,
@@ -17,50 +17,147 @@ export default function Library() {
     { title: "Albums" },
   ];
   let libraryCards = [
-    { title: "Playlists" },
+    {
+      title: "Playlists",
+      subtitle: "Your Playlists",
+      author: "You",
+      playlist: true,
+    },
+    {
+      title: "Playlists",
+      subtitle: "Your Playlists",
+      author: "You",
+      playlist: true,
+    },
+    {
+      title: "Playlists",
+      subtitle: "Your Playlists",
+      author: "You",
+      playlist: true,
+    },
     { title: "Artists" },
-    { title: "Albums" },
+    { title: "Artists" },
+    { title: "Artists" },
+    { title: "Artists" },
+    { title: "Albums", subtitle: "Your Albums", author: "You", playlist: true },
+    { title: "Albums", subtitle: "Your Albums", author: "You", playlist: true },
+    { title: "Albums", subtitle: "Your Albums", author: "You", playlist: true },
+    { title: "Albums", subtitle: "Your Albums", author: "You", playlist: true },
   ];
   let [activeLibrary, setActiveLibrary] = useState(true);
+  let [expandLibrary, setExpandLibrary] = useState(false);
+  let [searchLibrary, setSearchLibrary] = useState(false);
   return (
-    <div className="rounded-md bg-gray-900/40 max-w-80 pt-2">
+    <div
+      className={`rounded-md bg-gray-900/40 ${
+        expandLibrary ? "max-w-md" : activeLibrary ? "max-w-80" : "max-w-20"
+      } pt-2`}
+    >
       <div className="flex flex-col">
-        <div className="flex flex-row justify-between ps-2 pe-4">
+        <div
+          className={`flex flex-row ${
+            activeLibrary ? " justify-between ps-4" : "justify-center"
+          }`}
+        >
           <div className="flex flex-row items-center transition-all duration-200 ease-in-out text-gray-400 hover:text-white cursor-pointer px-2 py-1">
             {activeLibrary ? (
-              <div className="flex flex-row gap-x-2">
-                <ActiveCharBarIcon className="size-7 h-10" />
-                <div className="size-15 font-bold self-center">
-                  Your Library
-                </div>
+              <div
+                className="flex flex-row gap-x-2 items-center"
+                onClick={() => {
+                  setActiveLibrary(!activeLibrary);
+                }}
+              >
+                <ActiveCharBarIcon className="h-8" />
+                <div className="h-6 font-bold">Your Library</div>
               </div>
             ) : (
-              <ChartBarIcon className="size-7 h-10" />
+              <ChartBarIcon
+                className="h-8"
+                onClick={() => {
+                  setActiveLibrary(!activeLibrary);
+                }}
+              />
             )}
           </div>
-          <div className="flex flex-row gap-3 text-gray-400 self-center">
+          <div
+            className={`${
+              activeLibrary ? "flex flex-row gap-3 self-center" : "hidden"
+            } text-gray-400`}
+          >
             <PlusIcon className="size-6 h-10 transition-all duration-200 ease-in-out hover:text-white cursor-pointer" />
-            <ArrowRightIcon className="size-6 h-10  transition-all duration-200 ease-in-out hover:text-white cursor-pointer" />
+{ !expandLibrary ?
+            (<ArrowRightIcon
+              className="size-6 h-10  transition-all duration-200 ease-in-out hover:text-white cursor-pointer"
+              onClick={() => {
+                setExpandLibrary(!expandLibrary);
+              }}
+            />): (<ArrowLeftIcon className="size-6 h-10  transition-all duration-200 ease-in-out hover:text-white cursor-pointer"  onClick={() => {
+                setExpandLibrary(!expandLibrary);
+              }} />)
+}
           </div>
         </div>
-        <div className="flex flex-row gap-x-2 p-2 mx-1">
+        <div
+          className={`${
+            activeLibrary ? "flex flex-row gap-x-2 p-2 mx-1" : "hidden"
+          }`}
+        >
           {pills.map((pill, index) => (
             <Pill key={index} title={pill.title} />
           ))}
         </div>
         <div className="flex flex-col lg:h-[75vh] overflow-y-auto">
-          <div className="flex flex-row gap-x-2 p-2 mx-1 justify-between">
-            <div>
-              <MagnifyingGlassIcon className="h-6 text-gray-400 transition-all duration-200 ease-in-out hover:text-white cursor-pointer" />
-              <input type="text" placeholder="" className="hidden" />
+          <div className="flex flex-row gap-x-1 p-2 mx-1 justify-between">
+            <div
+              className={`${
+                activeLibrary
+                  ? `flex gap-x-1 p-1 transition-all delay-100 duration-200 ease-in-out rounded-md text-gray-400 ${
+                      searchLibrary ? "bg-slate-800" : ""
+                    }`
+                  : "hidden"
+              }`}
+            >
+              <MagnifyingGlassIcon
+                onClick={() => {
+                  setSearchLibrary(!searchLibrary);
+                }}
+                className="h-8 p-1 text-gray-400 transition-all duration-200 ease-in-out hover:text-white cursor-pointer rounded-full hover:bg-inherit/95"
+              />
+              <input
+                type="text"
+                placeholder="Search in Your Library"
+                className={`${
+                  searchLibrary
+                    ? "opacity-100 z-1 translate-x-0 w-100"
+                    : "-translate-x-72 -z-0 opacity-0 w-0"
+                } ease-in-out delay-100 duration-100 transition-all bg-inherit outline-none`}
+              />
             </div>
-            <div className="flex flex-row self-center gap-x-2 text-gray-400 transition-all duration-200 ease-in-out hover:text-white hover:scale-y-105 cursor-pointer">
-              <div className="text-sm font-medium">Recents</div>
+            <div
+              className={`${
+                activeLibrary
+                  ? "flex flex-row align-middle self-center gap-x-2 text-gray-400 transition-all duration-200 ease-in-out hover:text-white hover:scale-y-105 cursor-pointer"
+                  : "hidden"
+              }`}
+            >
+              <div
+                className={`${
+                  searchLibrary ? "hidden" : "block"
+                } text-sm font-medium`}
+              >
+                Recents
+              </div>
               <ListBulletIcon className="h-6" />
             </div>
           </div>
           {libraryCards.map((libraryCard, index) => (
-            <LibraryCard key={index} {...libraryCard} />
+            <LibraryCard
+              key={index}
+              {...libraryCard}
+              activeLibrary={activeLibrary}
+              expandLibrary={expandLibrary}
+              searchLibrary={searchLibrary}
+            />
           ))}
         </div>
       </div>
--- src/components/LibraryCard.jsx
+++ src/components/LibraryCard.jsx
@@ -1,16 +1,39 @@
 import React from "react";
-
+import { PlayIcon } from "@heroicons/react/24/solid";
 export default function LibraryCard(props) {
+console.log(props)
   return (
-    <div className="flex flex-row gap-x-2 my-1 rounded-md mx-3 px-1 py-2 transition-all duration-200 ease-in-out hover:bg-gray-800/40 cursor-pointer">
-      <img
-        src="https://placehold.co/60x60"
-        alt={props.title}
-        className="rounded-md select-none cursor-pointer"
-      />
+    <div className="flex flex-row gap-x-2 my-1 rounded-md mx-3 px-1 py-1 transition-all duration-200 ease-in-out hover:bg-gray-800/40 cursor-pointer group">
       <div className="flex flex-col self-center">
+        <div
+          className={`${
+            props.activeLibrary
+              ? "h-[50px] w-[50px] absolute bg-slate-400 bg-opacity-25 bg-clip-content group-hover:block hidden transition-all duration-100 ease-in-out"
+              : "hidden"
+          }`}
+        >
+          <PlayIcon className={`${props.activeLibrary?"text-white scale-50 hover:scale-[.6] transition-all duration-100 ease-in-out": ""}`} />
+        </div>
+        <img
+          src={`https://placehold.co/50x50?text=${props.title}`}
+          alt={props.title}
+          className={`${
+            props.playlist ? "rounded-md" : "rounded-full"
+          } select-none cursor-pointer`}
+        />
+      </div>
+      <div className={`${props.activeLibrary?"flex flex-col self-center" : "hidden"}`}>
         <div className="text-white font-semibold">{props.title}</div>
-        <div className="text-gray-300 text-sm font-light">{props.title}</div>
+
+        <div className="flex text-sm text-gray-400/95 gap-x-1 font-medium">
+          <div className="">{props.title}</div>
+          {props.subtitle && (
+            <>
+              <div>•</div>
+              <div className="">{props.subtitle}</div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
